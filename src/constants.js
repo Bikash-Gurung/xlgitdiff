7dfb4a499c43ae6165fc8a9f95d37d470ac71330
Badri Paudel <thecrazymanav@gmail.com>
2025-05-30 18:31:30 +0545

    export style changed instead of named export to support common js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,9 @@
-export const RUNNING_COMMAND = 'xldiff.diffxl';
-export const VIEW_OPTION_COMMAND = 'xldiff.viewfile';
-export const VIEW_OPTION_DISPLAY_NAME = 'Excel Diff Viewer';
+const RUNNING_COMMAND = 'xldiff.diffxl';
+const VIEW_OPTION_COMMAND = 'xldiff.viewfile';
+const VIEW_OPTION_DISPLAY_NAME = 'Excel Diff Viewer';
+
+module.exports = {
+    RUNNING_COMMAND, 
+    VIEW_OPTION_COMMAND,
+    VIEW_OPTION_DISPLAY_NAME
+}
