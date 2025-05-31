842bd3da7ec5082d23d65dcf792d44b9e474ca46
Badri Paudel <thecrazymanav@gmail.com>
2025-05-31 13:11:05 +0545

    Common logic refactored to a common function

--- extension.js
+++ extension.js
@@ -10,71 +10,14 @@ function activate(context) {
     RUNNING_COMMAND,
     async () => {
       const excelFilePath = excelUtils.getCurrentExcelFilePath();
-
-      if (excelFilePath) {
-        const currentJson = await excelUtils.readCurrentExcelData(
-          excelFilePath
-        );
-        const committedJson = await excelUtils.readCommittedExcelData(
-          excelFilePath
-        );
-
-        if (!currentJson) {
-            vscode.window.showErrorMessage("Failed to read the current Excel data.");
-            return;
-        }
-        if (!committedJson) {
-            // vscode.window.showErrorMessage("Failed to read the committed Excel data.");
-            return;
-        }
-
-        const differences = diffEngine.compareExcelJson(
-          currentJson,
-          committedJson
-        );
-        const summary = diffEngine.getDifferencesSummary(differences);
-
-        //console.log("Current Sheet Data:", currentJson);
-        //console.log("Committed Sheet Data:", committedJson);
-        //console.log("Comparison Summary:", summary);
-        //console.log("Differences:", differences);
-
-        const panel = vscode.window.createWebviewPanel(
-          VIEW_OPTION_COMMAND,
-          VIEW_OPTION_DISPLAY_NAME,
-           vscode.ViewColumn.One,
-          {
-            enableScripts: true,
-            retainContextWhenHidden: true,
-          }
-        );
-
-        panel.webview.html = getWebviewContent(
-          currentJson,
-          committedJson,
-          differences,
-          excelFilePath,
-          panel.webview,
-          context.extensionUri
-        );
-      }
+      await renderExcelViews(excelFilePath, context);
     }
   );
 
   // register excel viewer
   const provider = getProvider(context);
+  registerExcelAsViewOption(context, provider);
 
-  context.subscriptions.push(
-    vscode.window.registerCustomEditorProvider(
-      VIEW_OPTION_COMMAND,
-      provider,
-      {
-        webviewOptions: {
-          retainContextWhenHidden: true
-        }
-      }
-    )
-  );
   context.subscriptions.push(disposable);
 }
 
@@ -88,39 +31,62 @@ function getProvider(context) {
               enableScripts: true,
           };
           const excelFilePath = document.uri.fsPath;
-          if (excelFilePath) {
-            const currentJson = await excelUtils.readCurrentExcelData(excelFilePath);
-            const committedJson = await excelUtils.readCommittedExcelData(excelFilePath);
-            if (!currentJson) {
-                vscode.window.showErrorMessage("Failed to read the current Excel data.");
-                return;
-            }
-            if (!committedJson) {
-                // vscode.window.showErrorMessage("Failed to read the committed Excel data.");
-                return;
-            }
-            const differences = diffEngine.compareExcelJson(currentJson, committedJson);
-            const panel = vscode.window.createWebviewPanel(
-              VIEW_OPTION_COMMAND,
-              VIEW_OPTION_DISPLAY_NAME,
-              vscode.ViewColumn.One,
-              {
-               enableScripts: true,
-               retainContextWhenHidden: true,
-              }
-           );
-           panel.webview.html = getWebviewContent(
-             currentJson,
-             committedJson,
-             differences,
-             excelFilePath,
-             panel.webview,
-             context.extensionUri
-          );
-        }
+          await renderExcelViews(excelFilePath, context);
       }
   }
   return provider;
+}
+
+async function renderExcelViews(excelFilePath, context) {
+    if (!excelFilePath) {
+        vscode.window.showWarningMessage("Excel file might not have been selected.");
+        return;
+    }
+    const currentJson = await excelUtils.readCurrentExcelData(excelFilePath);
+    const committedJson = await excelUtils.readCommittedExcelData(excelFilePath);
+
+    if (!currentJson) {
+        vscode.window.showErrorMessage("Failed to read the current Excel data.");
+        return;
+    }
+    if (!committedJson) {
+        return;
+    }
+    const differences = diffEngine.compareExcelJson(currentJson, committedJson);
+    const summary = diffEngine.getDifferencesSummary(differences);
+
+    const panel = vscode.window.createWebviewPanel(
+      VIEW_OPTION_COMMAND,
+      VIEW_OPTION_DISPLAY_NAME,
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true,
+        retainContextWhenHidden: true,
+      }
+    );
+    
+    panel.webview.html = getWebviewContent(
+      currentJson,
+      committedJson,
+      differences,
+      excelFilePath,
+      panel.webview,
+      context.extensionUri
+    );
+}
+
+registerExcelAsViewOption = (context, provider) => {
+  context.subscriptions.push(
+    vscode.window.registerCustomEditorProvider(
+      VIEW_OPTION_COMMAND,
+      provider,
+      {
+        webviewOptions: {
+          retainContextWhenHidden: true
+        }
+      }
+    )
+  );
 }
 
 function deactivate() {
